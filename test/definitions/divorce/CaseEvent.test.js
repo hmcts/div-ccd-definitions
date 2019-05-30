e0a7c1b8593cc0ddfae3fcb572a274c7392334ec
Hosain <hosain.alahmad@hmcts.net>
2019-05-30 16:30:08 +0100

    DIV-4849 - revert change to test file

--- test/definitions/divorce/CaseEvent.test.js
+++ test/definitions/divorce/CaseEvent.test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-
+//sample test
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
