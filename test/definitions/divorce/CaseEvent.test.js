7823095cdd908db23d79d4b72d038f8ac82ba03d
Hosain <hosain.alahmad@hmcts.net>
2019-06-21 11:55:12 +0100

    DIV-4879 - added eslint

--- test/definitions/divorce/CaseEvent.test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-//sample test
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
--- test/definitions/divorce/CaseTypeTab.test.js
+++ test/definitions/divorce/CaseTypeTab.test.js
@@ -6,104 +6,101 @@ const caseField = require('../../../definitions/divorce/json/CaseField');
 
 const tabIds = uniq(map(caseTypeTab, 'TabID'));
 
-describe('CaseTypeTab', function() {
-  describe('duplicates', function() {
-    it('should contain a unique case field ID per tab ID (no duplicate field in a tab)', function() {
-      const uniqResult = uniqWith(
-        caseTypeTab,
-        (field1, field2) =>
-          field1.TabID === field2.TabID &&
-          field1.CaseFieldID === field2.CaseFieldID
-      );
-      expect(uniqResult).to.eql(caseTypeTab);
-    });
-    it('should contain a unique tab field display order ID field tab ID (no duplicate field order in a tab)', function() {
-      tabIds.forEach(tabId => {
-        const allFieldsPerTab = filter(caseTypeTab, field => field.TabID === tabId);
-        const uniqResults = uniqWith(
-          allFieldsPerTab,
-          (field1, field2) =>
-            field1.TabFieldDisplayOrder === field2.TabFieldDisplayOrder
-        );
-        expect(uniqResults).to.eql(allFieldsPerTab);
-      });
-    });
+describe('CaseTypeTab', () => {
+  it('should contain a unique case field ID per tab ID (no duplicate field in a tab)', () => {
+    const uniqResult = uniqWith(
+      caseTypeTab,
+      (field1, field2) => {
+        return field1.TabID === field2.TabID && field1.CaseFieldID === field2.CaseFieldID;
+      }
+    );
+    expect(uniqResult).to.eql(caseTypeTab);
   });
-
-  describe('sequence', function() {
-    it('should contain a proper sequence for TabFieldDisplayOrder with no gaps', function() {
-      tabIds.forEach(tabId => {
-        const allFieldsPerTab = filter(caseTypeTab, (field) => {
-          return field.TabID === tabId;
-        });
-        const allTabFieldDisplayOrderNumbers = map(allFieldsPerTab, (field) => {
-          return field.TabFieldDisplayOrder;
-        }).sort((a, b) => a - b);
-        for(let i = 1; i < allTabFieldDisplayOrderNumbers.length; i++) {
-          if(allTabFieldDisplayOrderNumbers[i] - allTabFieldDisplayOrderNumbers[i-1] !== 1) {
-            assert.fail(`Missing/unordered TabFieldDisplayOrder sequence number in TabID ${tabId} - expected ${allTabFieldDisplayOrderNumbers[i-1] + 1} but got ${allTabFieldDisplayOrderNumbers[i]}`);
-          }
-        }
-      });
-    });
-
-    const expected = {
-      History: 1,
-      petitionDetails: 2,
-      aosDetails: 3,
-      dnDetails: 4,
-      outcomeOfDnDetails: 5,
-      paymentDetails: 6,
-      documents: 7,
-      confidentialPetitioner: 8,
-      confidentialRespondent: 9,
-      confidentialCoRespondent: 10,
-      notes: 11,
-      marriageCertificate: 12,
-      coRespondent: 13,
-      SolicitorCoRespondent: 14,
-      LinkedCase: 15,
-      General: 1
-    };
+  it('should contain a unique tab field display order ID field tab ID (no duplicate field order in a tab)', () => {
     tabIds.forEach(tabId => {
-      it(`all ${tabId} fields should have the expected tab order ${expected[tabId]}`, function() {
-
-        const allTabFields = uniq(filter(caseTypeTab, (field) => {
-          return field.TabID === tabId;
-        }));
-        const allTabOrders = uniq(map(allTabFields, 'TabDisplayOrder'));
-        expect(allTabOrders).to.eql([expected[tabId]]);
+      const allFieldsPerTab = filter(caseTypeTab, field => {
+        return field.TabID === tabId;
       });
+      const uniqResults = uniqWith(
+        allFieldsPerTab,
+        (field1, field2) => {
+          return field1.TabFieldDisplayOrder === field2.TabFieldDisplayOrder;
+        }
+      );
+      expect(uniqResults).to.eql(allFieldsPerTab);
+    });
+  });
+  it('should contain a proper sequence for TabFieldDisplayOrder with no gaps', () => {
+    tabIds.forEach(tabId => {
+      const allFieldsPerTab = filter(caseTypeTab, field => {
+        return field.TabID === tabId;
+      });
+      const allTabFieldDisplayOrderNumbers = map(allFieldsPerTab, field => {
+        return field.TabFieldDisplayOrder;
+      }).sort((a, b) => {
+        return a - b;
+      });
+      for (let i = 1; i < allTabFieldDisplayOrderNumbers.length; i++) {
+        if (allTabFieldDisplayOrderNumbers[i] - allTabFieldDisplayOrderNumbers[i - 1] !== 1) {
+          assert.fail(`Missing/unordered TabFieldDisplayOrder sequence number in TabID ${tabId} - expected ${allTabFieldDisplayOrderNumbers[i - 1] + 1} but got ${allTabFieldDisplayOrderNumbers[i]}`);
+        }
+      }
     });
   });
 
-  describe('validation', function() {
-    it('should contain a valid Tab IDs', function() {
-      expect(tabIds).to.eql([
-        'History',
-        'petitionDetails',
-        'dnDetails',
-        'outcomeOfDnDetails',
-        'aosDetails',
-        'paymentDetails',
-        'documents',
-        'confidentialPetitioner',
-        'confidentialRespondent',
-        'confidentialCoRespondent',
-        'notes',
-        'marriageCertificate',
-        'coRespondent',
-        'SolicitorCoRespondent',
-        'LinkedCase',
-        'General'
-      ]);
+  const expected = {
+    History: 1,
+    petitionDetails: 2,
+    aosDetails: 3,
+    dnDetails: 4,
+    outcomeOfDnDetails: 5,
+    paymentDetails: 6,
+    documents: 7,
+    confidentialPetitioner: 8,
+    confidentialRespondent: 9,
+    confidentialCoRespondent: 10,
+    notes: 11,
+    marriageCertificate: 12,
+    coRespondent: 13,
+    SolicitorCoRespondent: 14,
+    LinkedCase: 15,
+    General: 1
+  };
+  tabIds.forEach(tabId => {
+    it(`all ${tabId} fields should have the expected tab order ${expected[tabId]}`, () => {
+      const allTabFields = uniq(filter(caseTypeTab, field => {
+        return field.TabID === tabId;
+      }));
+      const allTabOrders = uniq(map(allTabFields, 'TabDisplayOrder'));
+      expect(allTabOrders).to.eql([expected[tabId]]);
     });
-    it('should contain a valid case field IDs', function() {
-      const validFields = uniq(map(caseField, 'ID'));
-      let objectsWithInvalidCaseId = filter(caseTypeTab, (field) => {
-        return validFields.indexOf(field.CaseFieldID) === -1;
-      });
-      expect(objectsWithInvalidCaseId).to.eql([]);
+  });
+
+  it('should contain a valid Tab IDs', () => {
+    expect(tabIds).to.eql([
+      'History',
+      'petitionDetails',
+      'dnDetails',
+      'outcomeOfDnDetails',
+      'aosDetails',
+      'paymentDetails',
+      'documents',
+      'confidentialPetitioner',
+      'confidentialRespondent',
+      'confidentialCoRespondent',
+      'notes',
+      'marriageCertificate',
+      'coRespondent',
+      'SolicitorCoRespondent',
+      'LinkedCase',
+      'General'
+    ]);
+  });
+  it('should contain a valid case field IDs', () => {
+    const validFields = uniq(map(caseField, 'ID'));
+    const objectsWithInvalidCaseId = filter(caseTypeTab, field => {
+      return validFields.indexOf(field.CaseFieldID) === -1;
     });
+    expect(objectsWithInvalidCaseId).to.eql([]);
   });
 });
