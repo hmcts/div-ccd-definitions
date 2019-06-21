35aa6fe467381cfd83c2662a8c7585a07b6373f1
Hosain <hosain.alahmad@hmcts.net>
2019-06-21 15:59:57 +0100

    DIV-4850 - added test app to be able to deploy docker/install CCD chart

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express();
+const payload = {message: 'Im all right !'};
+const config = require('@hmcts/properties-volume').addTo(require('config'));
+const port = config.get('server.port');
+
+const healthcheck = require('@hmcts/nodejs-healthcheck');
+healthcheck.addTo(app,
+  {
+    checks: {
+      secretsCheck: healthcheck.raw(() => checkForSecrets() ? healthcheck.up() : healthcheck.down())
+    },
+    buildInfo: {
+      'chart-testing': 'nodejs-chart test'
+    }
+  });
+
+function checkForSecrets() {
+  try {
+    return true
+  } catch (error) {
+    console.log(`ERROR:` + error);
+
+    return false
+  }
+}
+
+app.get('/', (req, res) => res.send(payload))
+  .listen(port, () => console.log(`chart-nodeJs test app listening on http://0.0.0.0:${port}`))
