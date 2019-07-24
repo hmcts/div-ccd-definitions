f072cf22c210eb98a50fb98e470f315952f09e29
Alex Wakeman <wakemana@gmail.com>
2019-07-24 17:44:15 +0100

    Add in case event authorisation validation in unit test

--- test/unit/definitions/divorce/UserEventAuthorisation.test.js
+++ test/unit/definitions/divorce/UserEventAuthorisation.test.js
@@ -1,18 +1,21 @@
 /* eslint-disable */
 
 const expect = require('chai').expect;
-const assert = require('chai').assert;
 const { differenceWith, intersectionWith, concat } = require('lodash');
 
 const caseEventsAuthTab = Object.assign(require('definitions/divorce/json/AuthorisationCaseEvent'), []);
 const caseEventFields = Object.assign(require('definitions/divorce/json/CaseEventToFields'), []);
 const caseFieldAuthTab = Object.assign(require('definitions/divorce/json/AuthorisationCaseField'), []);
+const caseRolesTab = Object.assign(require('definitions/divorce/json/CaseRoles'), []);
 let caseEventsActive = [];
 
 describe('Events authorisation', () => {
-  function matchEventFieldToAuthField(authFieldEntry, eventCaseField, userRole) {
+  function matchEventFieldToAuthField(userRole, caseType) {
     return (authFieldEntry, eventCaseField) => {
-      if (eventCaseField.CaseFieldID === authFieldEntry.CaseFieldID && authFieldEntry.UserRole === userRole) {
+      if (eventCaseField.CaseFieldID === authFieldEntry.CaseFieldID
+        && authFieldEntry.UserRole === userRole
+        && authFieldEntry.CaseTypeID === caseType
+        && eventCaseField.CaseTypeID === caseType) {
         return true;
       }
     }
@@ -22,16 +25,26 @@ describe('Events authorisation', () => {
     caseEventsActive = caseEventsAuthTab.filter(entry => {
       return entry.CRUD === 'CRU' || entry.CRUD === 'RU';
     });
+
+    // we need to exclude the Case Roles events as its not used for Field Authorisation (is User Role only)
+    caseEventsActive = differenceWith(caseEventsActive, caseRolesTab, (eventActive, caseRole) => {
+      if (eventActive.UserRole === caseRole.ID) {
+        return true;
+      }
+    })
   });
 
   it('should have at least CRU or RU access level for all MANDATORY, OPTIONAL and READONLY show/hide event fields', () => {
     caseEventsActive.forEach(eventAuth => {
       const userRole = eventAuth.UserRole;
       const eventName = eventAuth.CaseEventID;
+      const caseType = eventAuth.CaseTypeID;
 
       // get all MANDATORY and OPTIONAL fields for this event
       let caseFieldsForEvent = caseEventFields.filter(entry => {
-        return entry.CaseEventID === eventName && (entry.DisplayContext === 'MANDATORY' || entry.DisplayContext === 'OPTIONAL');
+        return entry.CaseEventID === eventName
+          && (entry.DisplayContext === 'MANDATORY' || entry.DisplayContext === 'OPTIONAL')
+          && entry.CaseTypeID === caseType;
       });
 
       // get all the READONLY fields that are used as show/hide conditions (not labels) - these are sent with the event too
@@ -46,22 +59,22 @@ describe('Events authorisation', () => {
 
       // find the intersection between the event fields and the field's authorisations for this user role and event
       const relevantCaseFieldsAuth = intersectionWith(
-        caseFieldsForEvent, caseFieldAuthTab, matchEventFieldToAuthField(userRole));
+        caseFieldAuthTab, caseFieldsForEvent, matchEventFieldToAuthField(userRole, caseType));
 
       if (relevantCaseFieldsAuth.length !== caseFieldsForEvent.length) {
-        const diffFields = differenceWith(caseFieldsForEvent, relevantCaseFieldsAuth, matchEventFieldToAuthField(userRole));
-        // eslint-disable-next-line
+        const diffFields = differenceWith(caseFieldsForEvent, relevantCaseFieldsAuth, (eventCaseField, authFieldEntry) => {
+          if (eventCaseField.CaseFieldID === authFieldEntry.CaseFieldID
+            && authFieldEntry.UserRole === userRole
+            && authFieldEntry.CaseTypeID === caseType
+            && eventCaseField.CaseTypeID === caseType) {
+            return true;
+          }
+        });
         console.log(`Event ID: ${eventName} for ${userRole} user role is missing field authorisations`);
         console.dir(diffFields);
       }
 
       expect(relevantCaseFieldsAuth.length).to.eql(caseFieldsForEvent.length)
-
-      // const authFieldsFromTab = caseFieldAuthTab.filter(entry => {
-      //   return
-      // });
     });
   });
-
-
 });
