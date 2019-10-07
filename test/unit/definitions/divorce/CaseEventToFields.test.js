c7773a6ea229409cf03325a557442efc1a9c885b
Hosain <hosain.alahmad@hmcts.net>
2019-10-07 14:45:35 +0100

    DIV-5864 - added validation tests for CaseEventToFields

---
+++ test/unit/definitions/divorce/CaseEventToFields.test.js
@@ -0,0 +1,36 @@
+const { expect, assert } = require('chai');
+
+const { find } = require('lodash');
+
+const caseEvent = Object.assign(require('definitions/divorce/json/CaseEvent'), []);
+const caseField = Object.assign(require('definitions/divorce/json/CaseField'), []);
+const caseEventToFeilds = Object.assign(require('definitions/divorce/json/CaseEventToFields'), []);
+
+describe('CaseEventToFields', () => {
+  it('should contain valid event IDs', () => {
+    const errors = [];
+    caseEventToFeilds.forEach(caseEventToFieldsEntry => {
+      try {
+        expect(find(caseEvent, ['ID', caseEventToFieldsEntry.CaseEventID])).to.be.an('object');
+      } catch (error) {
+        errors.push(`Event ID ${caseEventToFieldsEntry.CaseEventID} is not valid`);
+      }
+    });
+    if (errors.length) {
+      assert.fail(`Found invalid case IDs - ${errors}`);
+    }
+  });
+  it('should contain valid field IDs', () => {
+    const errors = [];
+    caseEventToFeilds.forEach(caseEventToFieldsEntry => {
+      try {
+        expect(find(caseField, ['ID', caseEventToFieldsEntry.CaseFieldID])).to.be.an('object');
+      } catch (error) {
+        errors.push(`Field ID ${caseEventToFieldsEntry.CaseFieldID} is not valid`);
+      }
+    });
+    if (errors.length) {
+      assert.fail(`Found invalid field IDs - ${errors}`);
+    }
+  });
+});
