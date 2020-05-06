2fbc7a89c7aff628af9d4dcbc55976758ccc6756
Patryk Jar <jar.patryk@gmail.com>
2020-05-06 15:09:47 +0200

    refactor AuthorisationCaseEvent.test

--- test/unit/definitions/divorce/AuthorisationCaseEvent.test.js
+++ test/unit/definitions/divorce/AuthorisationCaseEvent.test.js
@@ -1,17 +1,33 @@
 const expect = require('chai').expect;
 const { uniqWith } = require('lodash');
 
-const caseTypeTab = Object.assign(require('definitions/divorce/json/AuthorisationCaseEvent/AuthorisationCaseEvent'), {});
+const authCaseEventCommon = Object.assign(require('definitions/divorce/json/AuthorisationCaseEvent/AuthorisationCaseEvent'), {});
+
+function isDuplicated(field1, field2) {
+  return field1.CaseTypeID === field2.CaseTypeID
+    && field1.CaseEventID === field2.CaseEventID
+    && field1.UserRole === field2.UserRole;
+}
+
+function mergeJsonFilesFor(whatEnvs) {
+  const authCaseEventForSpecificEnvs = Object
+    .assign(require(`definitions/divorce/json/AuthorisationCaseEvent/AuthorisationCaseEvent-${whatEnvs}`), {});
+
+  return [...authCaseEventCommon, ...authCaseEventForSpecificEnvs];
+}
 
 describe('AuthorisationCaseEvent', () => {
-  it('should contain a unique case type, case event ID and role (no duplicates)', () => {
-    const uniqResult = uniqWith(
-      caseTypeTab,
-      (field1, field2) => {
-        return field1.CaseTypeID === field2.CaseTypeID && field1.CaseEventID === field2.CaseEventID
-            && field1.UserRole === field2.UserRole;
-      }
-    );
-    expect(uniqResult).to.eql(caseTypeTab);
+  it('should contain a unique case type, case event ID and role (no duplicates) for non-prod', () => {
+    const nonProd = mergeJsonFilesFor('nonprod');
+    const uniqResult = uniqWith(nonProd, isDuplicated);
+
+    expect(uniqResult).to.eql(nonProd);
+  });
+
+  it('should contain a unique case type, case event ID and role (no duplicates) for prod only', () => {
+    const prodOnly = mergeJsonFilesFor('only-for-prod');
+    const uniqResult = uniqWith(prodOnly, isDuplicated);
+
+    expect(uniqResult).to.eql(prodOnly);
   });
 });
