b86ab55d80c0db9c2f9c7593e7356c91931e1faf
Felix Eyetan <peyetan@yahoo.com>
2020-07-07 13:46:42 +0100

    DIV-6153 Extend amend petition for refusal sol (#404)
    * Adding Explanatory start page definitions
    
    * Updating start page header style
    
    * Updating amend page layout index
    
    * Update to case state authorisation for solicitor
    
    * Adding permission for caseworker-divorce-solicitor role for amendPetitionForRefusalSol event
    
    * Fixing duplicate errors in build
    
    * Removing white space
    
    * Updated content in new info for header section
    
    * Code Review: Adding authorisation case field entries for new labels
    * Updating formatting for Before you start header
    * Moved case event to fields entries
    
    * Updating caseevents to remove page labels
    
    * Renaming label property to follow common convention in definition file
    
    * * Updating existing event with more precondition states
    * Ensuring that the precondition state allow solicitor the authorisation to update cases in these states
    
    * WIP-Setting up nonprod files for extending events
    
    * * Updating existing event with required solicitor permission
    
    * Update to allow  test to pass for -prod and -nonprod files in AuthorisationCaseState folder
    
    * Update to test output message
    
    * Fixing merge conflict issues after merging previous related PRs

--- test/unit/definitions/divorce/AuthorisationCaseState.test.js
+++ test/unit/definitions/divorce/AuthorisationCaseState.test.js
@@ -1,52 +1,34 @@
 const expect = require('chai').expect;
 const { uniqWith } = require('lodash');
-const fs = require('fs');
 
 const load = require;
-const authCaseState = Object.assign(require('definitions/divorce/json/AuthorisationCaseState/AuthorisationCaseState'), {});
+const authCaseStateCommon = Object.assign(require('definitions/divorce/json/AuthorisationCaseState/AuthorisationCaseState'), {});
 
 function isDuplicated(field1, field2) {
-  return field1.CaseTypeID === field2.CaseTypeID
-    && field1.CaseStateID === field2.CaseStateID
-    && field1.UserRole === field2.UserRole;
+    return field1.CaseTypeID === field2.CaseTypeID
+        && field1.CaseStateID === field2.CaseStateID
+        && field1.UserRole === field2.UserRole;
 }
 
-function mergeJsonFilesFor(whatFolder, nonProdStartsWith) {
-  const files = fs.readdirSync(whatFolder);
-  const authCaseStateNonProd = [];
+function mergeJsonFilesFor(whatEnvs) {
+    const authCaseForSpecificEnvs = Object
+        .assign(load(`definitions/divorce/json/AuthorisationCaseState/AuthorisationCaseState-${whatEnvs}`), {});
 
-  files.forEach(filename => {
-    const filenameRegEx = new RegExp(`${nonProdStartsWith}.*.json`, 'i');
-    if (filename.match(filenameRegEx)) {
-       const nonProdFile = Object
-          .assign(load(`${whatFolder}/${filename}`), {});
-       authCaseStateNonProd.push(...nonProdFile);
-    }
-  });
-
-  return [...authCaseState, ...authCaseStateNonProd];
+    return [...authCaseStateCommon, ...authCaseForSpecificEnvs];
 }
 
 describe('AuthorisationCaseState', () => {
-  if (fs.existsSync('definitions/divorce/json/AuthorisationCaseState/')){
-     it('should contain a unique case state ID, case type ID and role (no duplicates) for non prod files', () => {
-        const jsonConfigFiles = mergeJsonFilesFor('definitions/divorce/json/AuthorisationCaseState/', 'AuthorisationCaseState-');
-        const uniqResult = uniqWith(jsonConfigFiles, isDuplicated);
+    it('should contain a unique case state, case type ID and role (no duplicates) for nonprod files', () => {
+        const nonProd = mergeJsonFilesFor('nonprod');
+        const uniqResult = uniqWith(nonProd, isDuplicated);
 
-        expect(uniqResult).to.eql(jsonConfigFiles);
-     });
-  }
+        expect(uniqResult).to.eql(nonProd);
+    });
 
-  it('should contain a unique case state ID, case type ID and role (no duplicates) for prod file', () => {
-    const uniqResult = uniqWith(
-      authCaseState,
-      (field1, field2) => {
-        return field1.CaseTypeID === field2.CaseTypeID
-                   && field1.CaseStateID === field2.CaseStateID
-                   && field1.UserRole === field2.UserRole;
-      }
-    );
+    it('should contain a unique case state ID, case type ID and role (no duplicates) for prod file', () => {
+        const prodOnly = mergeJsonFilesFor('prod');
+        const uniqResult = uniqWith(prodOnly, isDuplicated);
 
-    expect(uniqResult).to.eql(authCaseState);
-  });
+        expect(uniqResult).to.eql(prodOnly);
+    });
 });
