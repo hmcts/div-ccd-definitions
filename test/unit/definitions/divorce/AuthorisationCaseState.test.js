554b82e04b66f58ed6933917aaf32c2560e85206
Marc-steeven Eyeni-Kantsey <mseyeni@icloud.com>
2020-07-09 13:59:52 +0100

    back to original changes (#405)
    Co-authored-by: mseyeni@icloud.com <marc-steevene@Ilya.lan>

--- test/unit/definitions/divorce/AuthorisationCaseState.test.js
+++ test/unit/definitions/divorce/AuthorisationCaseState.test.js
@@ -5,30 +5,30 @@ const load = require;
 const authCaseStateCommon = Object.assign(require('definitions/divorce/json/AuthorisationCaseState/AuthorisationCaseState'), {});
 
 function isDuplicated(field1, field2) {
-    return field1.CaseTypeID === field2.CaseTypeID
+  return field1.CaseTypeID === field2.CaseTypeID
         && field1.CaseStateID === field2.CaseStateID
         && field1.UserRole === field2.UserRole;
 }
 
 function mergeJsonFilesFor(whatEnvs) {
-    const authCaseForSpecificEnvs = Object
-        .assign(load(`definitions/divorce/json/AuthorisationCaseState/AuthorisationCaseState-${whatEnvs}`), {});
+  const authCaseForSpecificEnvs = Object
+    .assign(load(`definitions/divorce/json/AuthorisationCaseState/AuthorisationCaseState-${whatEnvs}`), {});
 
-    return [...authCaseStateCommon, ...authCaseForSpecificEnvs];
+  return [...authCaseStateCommon, ...authCaseForSpecificEnvs];
 }
 
 describe('AuthorisationCaseState', () => {
-    it('should contain a unique case state, case type ID and role (no duplicates) for nonprod files', () => {
-        const nonProd = mergeJsonFilesFor('nonprod');
-        const uniqResult = uniqWith(nonProd, isDuplicated);
+  it('should contain a unique case state, case type ID and role (no duplicates) for nonprod files', () => {
+    const nonProd = mergeJsonFilesFor('nonprod');
+    const uniqResult = uniqWith(nonProd, isDuplicated);
 
-        expect(uniqResult).to.eql(nonProd);
-    });
+    expect(uniqResult).to.eql(nonProd);
+  });
 
-    it('should contain a unique case state ID, case type ID and role (no duplicates) for prod file', () => {
-        const prodOnly = mergeJsonFilesFor('prod');
-        const uniqResult = uniqWith(prodOnly, isDuplicated);
+  it('should contain a unique case state ID, case type ID and role (no duplicates) for prod file', () => {
+    const prodOnly = mergeJsonFilesFor('prod');
+    const uniqResult = uniqWith(prodOnly, isDuplicated);
 
-        expect(uniqResult).to.eql(prodOnly);
-    });
+    expect(uniqResult).to.eql(prodOnly);
+  });
 });
