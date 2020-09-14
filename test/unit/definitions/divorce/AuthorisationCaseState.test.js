b8b6d2cbc7cd20203c2a630b0d363edd884675fe
Patryk Jar <jar.patryk@gmail.com>
2020-09-14 11:12:39 +0200

    AuthorisationCaseState must use existing states

--- test/unit/definitions/divorce/AuthorisationCaseState.test.js
+++ test/unit/definitions/divorce/AuthorisationCaseState.test.js
@@ -4,6 +4,8 @@ const { isFieldDuplicated } = require('../../utils/utils');
 
 const load = require;
 
+const states = load(`definitions/divorce/json/State/State.json`);
+
 function loadAllFiles(files) {
   let definitions = [];
 
@@ -15,7 +17,7 @@ function loadAllFiles(files) {
 }
 
 describe('AuthorisationCaseState', () => {
-  it('should contain a unique case state, case type ID and role (no duplicates) for nonprod files', () => {
+  describe('for nonprod files all definitions should', () => {
     const nonProd = loadAllFiles(
       [
         'AuthorisationCaseState',
@@ -24,18 +26,37 @@ describe('AuthorisationCaseState', () => {
         'AuthorisationCaseState-pet-amend-nonprod'
       ]
     );
-    const uniqResult = uniqWith(nonProd, isFieldDuplicated('CaseStateID'));
 
-    expect(uniqResult).to.eql(nonProd);
+    it('contain a unique case state, case type ID and role (no duplicates) for nonprod files', () => {
+      const uniqResult = uniqWith(nonProd, isFieldDuplicated('CaseStateID'));
+      expect(uniqResult).to.eql(nonProd);
+    });
+
+    it('use existing states', () => {
+      nonProd.forEach(authDefinition => {
+        expect(find(states, ['ID', authDefinition.CaseStateID])).to.be.an('object');
+      })
+    });
   });
 
-  it('should contain a unique case state ID, case type ID and role (no duplicates) for prod file', () => {
-    const prodOnly = loadAllFiles(
-      ['AuthorisationCaseState', 'AuthorisationCaseState-prod']
+  describe('for prod files all definitions should', () => {
+    const nonProd = loadAllFiles(
+      [
+        'AuthorisationCaseState',
+        'AuthorisationCaseState-prod',
+        'AuthorisationCaseState-pet-amend-prod'
+      ]
     );
 
-    const uniqResult = uniqWith(prodOnly, isFieldDuplicated('CaseStateID'));
+    it('contain a unique case state, case type ID and role (no duplicates)', () => {
+      const uniqResult = uniqWith(nonProd, isFieldDuplicated('CaseStateID'));
+      expect(uniqResult).to.eql(nonProd);
+    });
 
-    expect(uniqResult).to.eql(prodOnly);
+    it('use existing states ', () => {
+      nonProd.forEach(authDefinition => {
+        expect(find(states, ['ID', authDefinition.CaseStateID])).to.be.an('object');
+      })
+    });
   });
 });
