a4b4434a843967b8390bda05c7845f510a9e100d
Felix Eyetan <peyetan@yahoo.com>
2020-09-30 14:09:31 +0100

    DIV-6561-Adding-new-Service-Application-documents-collection-definition (#493)
    * Adding new definitions for service application documents
    * Refactoring case type tab test to handle multiple feature and files
    * Updating existing test suite

--- test/unit/definitions/divorce/CaseTypeTab.test.js
+++ test/unit/definitions/divorce/CaseTypeTab.test.js
@@ -1,29 +1,44 @@
-const expect = require('chai').expect;
-const assert = require('chai').assert;
+const { expect, assert } = require('chai');
 const { uniq, uniqWith, map, filter } = require('lodash');
 const { loadAllFiles, sortCaseTypeTabs } = require('../../utils/utils');
+const {
+  nonProdTabDisplayOrder,
+  nonProdTabIds,
+  prodTabDisplayOrder,
+  prodTabIds,
+  validateUniqueTabDisplayOrder,
+  validateTabFieldDisplayOrder
+} = require('../../utils/caseTabTypeHelper');
 
 const getAllCaseTypeTabDefinitions = loadAllFiles('CaseTypeTab');
-const caseTypeTab = getAllCaseTypeTabDefinitions(
-  [
-    'CaseTypeTab',
-    'CaseTypeTab-prod',
-    'CaseTypeTab-deemed-and-dispensed-nonprod'
-  ]);
+let caseTypeTab = [];
 
 const getAllCaseFieldDefinitions = loadAllFiles('CaseField');
-const caseField = getAllCaseFieldDefinitions(
-  [
-    'CaseField',
-    'CaseField-prod',
-    'CaseField-deemed-and-dispensed-nonprod'
-  ]);
+let caseField = [];
 
-const sortedCaseTabs = sortCaseTypeTabs(caseTypeTab);
-const tabIds = uniq(map(sortedCaseTabs, 'TabID'));
+let sortedCaseTabs = [];
+let tabIds = [];
 
-describe('CaseTypeTab', () => {
-  it('should contain a unique case field ID per tab ID (no duplicate field in a tab)', () => {
+describe('CaseTypeTab (nonprod)', () => {
+  before(() => {
+    caseTypeTab = getAllCaseTypeTabDefinitions(
+      [
+        'CaseTypeTab',
+        'CaseTypeTab-general-order-nonprod',
+        'CaseTypeTab-deemed-and-dispensed-nonprod'
+      ]);
+
+    caseField = getAllCaseFieldDefinitions(
+      [
+        'CaseField',
+        'CaseField-general-order-nonprod',
+        'CaseField-deemed-and-dispensed-nonprod'
+      ]);
+    sortedCaseTabs = sortCaseTypeTabs(caseTypeTab);
+    tabIds = uniq(map(sortedCaseTabs, 'TabID'));
+  });
+
+  it('should contain unique case field ID per tab ID (no duplicate field in a tab)', () => {
     const uniqResult = uniqWith(
       caseTypeTab,
       (field1, field2) => {
@@ -33,117 +48,145 @@ describe('CaseTypeTab', () => {
     expect(uniqResult).to.eql(caseTypeTab);
   });
 
-  it('should contain a unique tab field display order ID field tab ID (no duplicate field order in a tab)', () => {
-    tabIds.forEach(tabId => {
-      const allFieldsPerTab = filter(caseTypeTab, field => {
-        return field.TabID === tabId;
-      });
-      const uniqResults = uniqWith(
-        allFieldsPerTab,
-        (field1, field2) => {
-          return field1.TabFieldDisplayOrder === field2.TabFieldDisplayOrder;
-        }
-      );
-      expect(uniqResults).to.eql(allFieldsPerTab);
-    });
-  });
-
-  it('should contain a proper sequence for TabFieldDisplayOrder with no gaps', () => {
-    tabIds.forEach(tabId => {
-      const allFieldsPerTab = filter(caseTypeTab, field => {
-        return field.TabID === tabId;
-      });
-      const allTabFieldDisplayOrderNumbers = map(allFieldsPerTab, field => {
-        return field.TabFieldDisplayOrder;
-      }).sort((a, b) => {
-        return a - b;
-      });
-      for (let i = 1; i < allTabFieldDisplayOrderNumbers.length; i++) {
-        if (allTabFieldDisplayOrderNumbers[i] - allTabFieldDisplayOrderNumbers[i - 1] !== 1) {
-          assert.fail(`Missing/unordered TabFieldDisplayOrder sequence number in TabID ${tabId} - expected ${allTabFieldDisplayOrderNumbers[i - 1] + 1} but got ${allTabFieldDisplayOrderNumbers[i]}`);
-        }
-      }
-    });
-  });
-
-  const expected = {
-    History: 1,
-    petitionDetails: 2,
-    aosDetails: 3,
-    dnDetails: 4,
-    outcomeOfDnDetails: 5,
-    DecreeAbsolute: 6,
-    paymentDetailsCourtAdmin: 7,
-    paymentDetailsCourtAdminBeta: 8,
-    paymentDetailsCourtAdminLa: 9,
-    documents: 10,
-    confidentialPetitionerCourtAdmin: 11,
-    confidentialPetitionerCourtAdminBeta: 12,
-    confidentialPetitionerCourtAdminLa: 13,
-    confidentialRespondentCourtAdmin: 14,
-    confidentialRespondentCourtAdminBeta: 15,
-    confidentialRespondentCourtAdminLa: 16,
-    confidentialCoRespondentCourtAdmin: 17,
-    confidentialCoRespondentCourtAdminBeta: 18,
-    confidentialCoRespondentCourtAdminLa: 19,
-    notes: 20,
-    marriageCertificate: 21,
-    coRespondent: 22,
-    serviceApplication: 23,
-    LinkedCase: 24,
-    Language: 25,
-    ConfidentialDocuments: 26,
-    General: 1
-  };
-
   tabIds.forEach(tabId => {
-    it(`all ${tabId} fields should have the expected tab order ${expected[tabId]}`, () => {
+    it(`all ${tabId} fields should have the expected tab order ${nonProdTabDisplayOrder[tabId]}`, () => {
       const allTabFields = uniq(filter(caseTypeTab, field => {
         return field.TabID === tabId;
       }));
       const allTabOrders = uniq(map(allTabFields, 'TabDisplayOrder'));
-      expect(allTabOrders).to.eql([expected[tabId]]);
+      expect(allTabOrders).to.eql([nonProdTabDisplayOrder[tabId]]);
     });
   });
 
-  it('should contain a valid Tab IDs', () => {
-    expect(tabIds).to.eql([
-      'History',
-      'General',
-      'petitionDetails',
-      'aosDetails',
-      'dnDetails',
-      'outcomeOfDnDetails',
-      'DecreeAbsolute',
-      'paymentDetailsCourtAdmin',
-      'paymentDetailsCourtAdminBeta',
-      'paymentDetailsCourtAdminLa',
-      'documents',
-      'confidentialPetitionerCourtAdmin',
-      'confidentialPetitionerCourtAdminBeta',
-      'confidentialPetitionerCourtAdminLa',
-      'confidentialRespondentCourtAdmin',
-      'confidentialRespondentCourtAdminBeta',
-      'confidentialRespondentCourtAdminLa',
-      'confidentialCoRespondentCourtAdmin',
-      'confidentialCoRespondentCourtAdminBeta',
-      'confidentialCoRespondentCourtAdminLa',
-      'notes',
-      'marriageCertificate',
-      'coRespondent',
-      // 'SolicitorCoRespondent', - TODO - uncomment this when we go live with AOS pack 2
-      'serviceApplication',
-      'LinkedCase',
-      'Language',
-      'ConfidentialDocuments'
-    ]);
+  it('should contain valid Tab IDs', () => {
+    expect(tabIds).to.eql(nonProdTabIds);
   });
 
-  it('should contain a valid case field IDs', () => {
+  it('should contain valid case field IDs', () => {
     const validFields = uniq(map(caseField, 'ID'));
     const objectsWithInvalidCaseId = filter(caseTypeTab, field => {
       return validFields.indexOf(field.CaseFieldID) === -1;
     });
     expect(objectsWithInvalidCaseId).to.eql([]);
+  });
+});
+
+/**
+ * This suite (and sub suites) checks to make sure that there are no duplicate TabDisplayOrder or
+ * TabFieldDisplayOrder in the concatenated release definition.
+ * Note: As a post release activity the related release describe block can be removed
+ * as the feature file would no longer exist
+ */
+describe('CaseTypeTab duplicate checks (nonprod)', () => {
+  before(() => {
+    sortedCaseTabs = sortCaseTypeTabs(caseTypeTab);
+    tabIds = uniq(map(sortedCaseTabs, 'TabID'));
+  });
+
+  describe('General Order - CaseTypeTab check', () => {
+    before(() => {
+      caseTypeTab = getAllCaseTypeTabDefinitions(
+        [
+          'CaseTypeTab',
+          'CaseTypeTab-prod',
+          'CaseTypeTab-general-order-nonprod'
+        ]);
+    });
+
+    it('should contain unique tab field display order ID field tab ID (no duplicate field order in a tab)', () => {
+      const validationErrors = validateUniqueTabDisplayOrder(tabIds, caseTypeTab);
+      expect(validationErrors).to.have.lengthOf(0);
+    });
+
+    it('should contain proper sequence for TabFieldDisplayOrder with no gaps', () => {
+      assert.doesNotThrow(() => {
+        validateTabFieldDisplayOrder(tabIds, caseTypeTab);
+      },
+      /Missing\/unordered TabFieldDisplayOrder sequence number in TabID/);
+    });
+  });
+
+  describe('Deemed and Dispensed - CaseTypeTab check', () => {
+    before(() => {
+      caseTypeTab = getAllCaseTypeTabDefinitions(
+        [
+          'CaseTypeTab',
+          'CaseTypeTab-deemed-and-dispensed-nonprod'
+        ]);
+    });
+
+    it('should contain unique tab field display order ID field tab ID (no duplicate field order in a tab)', () => {
+      const validationErrors = validateUniqueTabDisplayOrder(tabIds, caseTypeTab);
+      expect(validationErrors).to.have.lengthOf(0);
+    });
+
+    it('should contain proper sequence for TabFieldDisplayOrder with no gaps', () => {
+      assert.doesNotThrow(() => {
+        validateTabFieldDisplayOrder(tabIds, caseTypeTab);
+      },
+      /Missing\/unordered TabFieldDisplayOrder sequence number in TabID/);
+    });
+  });
+});
+
+describe('CaseTypeTab (prod)', () => {
+  before(() => {
+    caseTypeTab = getAllCaseTypeTabDefinitions(
+      [
+        'CaseTypeTab',
+        'CaseTypeTab-prod'
+      ]);
+
+    caseField = getAllCaseFieldDefinitions(
+      [
+        'CaseField',
+        'CaseField-prod'
+      ]);
+    sortedCaseTabs = sortCaseTypeTabs(caseTypeTab);
+    tabIds = uniq(map(sortedCaseTabs, 'TabID'));
+  });
+
+  it('should contain unique case field ID per tab ID (no duplicate field in a tab)', () => {
+    const uniqResult = uniqWith(
+      caseTypeTab,
+      (field1, field2) => {
+        return field1.TabID === field2.TabID && field1.CaseFieldID === field2.CaseFieldID;
+      }
+    );
+    expect(uniqResult).to.eql(caseTypeTab);
+  });
+
+  tabIds.forEach(tabId => {
+    it(`all ${tabId} fields should have the expected tab order ${prodTabDisplayOrder[tabId]}`, () => {
+      const allTabFields = uniq(filter(caseTypeTab, field => {
+        return field.TabID === tabId;
+      }));
+      const allTabOrders = uniq(map(allTabFields, 'TabDisplayOrder'));
+      expect(allTabOrders).to.eql([prodTabDisplayOrder[tabId]]);
+    });
+  });
+
+  it('should contain valid Tab IDs', () => {
+    expect(tabIds).to.eql(prodTabIds);
+  });
+
+  it('should contain valid case field IDs', () => {
+    const validFields = uniq(map(caseField, 'ID'));
+    const objectsWithInvalidCaseId = filter(caseTypeTab, field => {
+      return validFields.indexOf(field.CaseFieldID) === -1;
+    });
+    expect(objectsWithInvalidCaseId).to.eql([]);
+  });
+
+  it('should contain unique tab field display order ID field tab ID (no duplicate field order in a tab)', () => {
+    const validationErrors = validateUniqueTabDisplayOrder(tabIds, caseTypeTab);
+    expect(validationErrors).to.have.lengthOf(0);
+  });
+
+  it('should contain proper sequence for TabFieldDisplayOrder with no gaps', () => {
+    assert.doesNotThrow(() => {
+      validateTabFieldDisplayOrder(tabIds, caseTypeTab);
+    },
+    /Missing\/unordered TabFieldDisplayOrder sequence number in TabID/);
   });
 });
---
+++ test/unit/utils/caseTabTypeHelper.js
@@ -0,0 +1,189 @@
+const { uniqWith, filter, isEqual, map } = require('lodash');
+const { assert } = require('chai');
+
+const nonProdTabDisplayOrder = {
+  History: 1,
+  petitionDetails: 2,
+  aosDetails: 3,
+  dnDetails: 4,
+  outcomeOfDnDetails: 5,
+  DecreeAbsolute: 6,
+  paymentDetailsCourtAdmin: 7,
+  paymentDetailsCourtAdminBeta: 8,
+  paymentDetailsCourtAdminLa: 9,
+  documents: 10,
+  confidentialPetitionerCourtAdmin: 11,
+  confidentialPetitionerCourtAdminBeta: 12,
+  confidentialPetitionerCourtAdminLa: 13,
+  confidentialRespondentCourtAdmin: 14,
+  confidentialRespondentCourtAdminBeta: 15,
+  confidentialRespondentCourtAdminLa: 16,
+  confidentialCoRespondentCourtAdmin: 17,
+  confidentialCoRespondentCourtAdminBeta: 18,
+  confidentialCoRespondentCourtAdminLa: 19,
+  notes: 20,
+  marriageCertificate: 21,
+  coRespondent: 22,
+  serviceApplication: 23,
+  LinkedCase: 24,
+  Language: 25,
+  ConfidentialDocuments: 26,
+  General: 1
+};
+
+const nonProdTabIds = [
+  'History',
+  'General',
+  'petitionDetails',
+  'aosDetails',
+  'dnDetails',
+  'outcomeOfDnDetails',
+  'DecreeAbsolute',
+  'paymentDetailsCourtAdmin',
+  'paymentDetailsCourtAdminBeta',
+  'paymentDetailsCourtAdminLa',
+  'documents',
+  'confidentialPetitionerCourtAdmin',
+  'confidentialPetitionerCourtAdminBeta',
+  'confidentialPetitionerCourtAdminLa',
+  'confidentialRespondentCourtAdmin',
+  'confidentialRespondentCourtAdminBeta',
+  'confidentialRespondentCourtAdminLa',
+  'confidentialCoRespondentCourtAdmin',
+  'confidentialCoRespondentCourtAdminBeta',
+  'confidentialCoRespondentCourtAdminLa',
+  'notes',
+  'marriageCertificate',
+  'coRespondent',
+  // 'SolicitorCoRespondent', - TODO - uncomment this when we go live with AOS pack 2
+  'serviceApplication',
+  'LinkedCase',
+  'Language',
+  'ConfidentialDocuments'
+];
+
+const prodTabDisplayOrder = {
+  History: 1,
+  petitionDetails: 2,
+  aosDetails: 3,
+  dnDetails: 4,
+  outcomeOfDnDetails: 5,
+  DecreeAbsolute: 6,
+  paymentDetailsCourtAdmin: 7,
+  paymentDetailsCourtAdminBeta: 8,
+  paymentDetailsCourtAdminLa: 9,
+  documents: 10,
+  confidentialPetitionerCourtAdmin: 11,
+  confidentialPetitionerCourtAdminBeta: 12,
+  confidentialPetitionerCourtAdminLa: 13,
+  confidentialRespondentCourtAdmin: 14,
+  confidentialRespondentCourtAdminBeta: 15,
+  confidentialRespondentCourtAdminLa: 16,
+  confidentialCoRespondentCourtAdmin: 17,
+  confidentialCoRespondentCourtAdminBeta: 18,
+  confidentialCoRespondentCourtAdminLa: 19,
+  notes: 20,
+  marriageCertificate: 21,
+  coRespondent: 22,
+  LinkedCase: 23,
+  Language: 24,
+  ConfidentialDocuments: 25,
+  General: 1
+};
+
+const prodTabIds = [
+  'History',
+  'General',
+  'petitionDetails',
+  'aosDetails',
+  'dnDetails',
+  'outcomeOfDnDetails',
+  'DecreeAbsolute',
+  'paymentDetailsCourtAdmin',
+  'paymentDetailsCourtAdminBeta',
+  'paymentDetailsCourtAdminLa',
+  'documents',
+  'confidentialPetitionerCourtAdmin',
+  'confidentialPetitionerCourtAdminBeta',
+  'confidentialPetitionerCourtAdminLa',
+  'confidentialRespondentCourtAdmin',
+  'confidentialRespondentCourtAdminBeta',
+  'confidentialRespondentCourtAdminLa',
+  'confidentialCoRespondentCourtAdmin',
+  'confidentialCoRespondentCourtAdminBeta',
+  'confidentialCoRespondentCourtAdminLa',
+  'notes',
+  'marriageCertificate',
+  'coRespondent',
+  // 'SolicitorCoRespondent', - TODO - uncomment this when we go live with AOS pack 2
+  'LinkedCase',
+  'Language',
+  'ConfidentialDocuments'
+];
+
+/**
+ * Validates the tab display ids to ensure there are no duplicate in the feature files
+ * @param tabIds List of case type TabID's
+ * @param caseTypeTab List of casetype tabs. A concatenation from all feature files
+ * @returns {[]} List with duplicates if found
+ */
+function validateUniqueTabDisplayOrder(tabIds, caseTypeTab) {
+  const errors = [];
+  let allFieldsPerTab = null;
+  let uniqResults = null;
+
+  tabIds.forEach(tabId => {
+    allFieldsPerTab = filter(caseTypeTab, field => {
+      return field.TabID === tabId;
+    });
+
+    uniqResults = uniqWith(
+      allFieldsPerTab,
+      (field1, field2) => {
+        return field1.TabFieldDisplayOrder === field2.TabFieldDisplayOrder;
+      });
+
+    if (!isEqual(allFieldsPerTab, uniqResults)) {
+      errors.push(uniqResults);
+    }
+  });
+
+  return errors;
+}
+
+/**
+ * Validates the tab field display order to ensure there are no gaps in feature files.
+ * Would throw an error if a duplicate tab display id is found
+ * @param tabIds List of case type TabID's
+ * @param caseTypeTab caseTypeTab List of casetype tabs. A concatenation from all feature files
+ */
+function validateTabFieldDisplayOrder(tabIds, caseTypeTab) {
+  let allFieldsPerTab = [];
+  let allTabFieldDisplayOrderNumbers = [];
+
+  tabIds.forEach(tabId => {
+    allFieldsPerTab = filter(caseTypeTab, field => {
+      return field.TabID === tabId;
+    });
+    allTabFieldDisplayOrderNumbers = map(allFieldsPerTab, field => {
+      return field.TabFieldDisplayOrder;
+    })
+      .sort((a, b) => {
+        return a - b;
+      });
+    for (let i = 1; i < allTabFieldDisplayOrderNumbers.length; i++) {
+      if (allTabFieldDisplayOrderNumbers[i] - allTabFieldDisplayOrderNumbers[i - 1] !== 1) {
+        assert.fail(`Missing/unordered TabFieldDisplayOrder sequence number in TabID ${tabId} - expected ${allTabFieldDisplayOrderNumbers[i - 1] + 1} but got ${allTabFieldDisplayOrderNumbers[i]}`);
+      }
+    }
+  });
+}
+
+module.exports = {
+  nonProdTabDisplayOrder,
+  nonProdTabIds,
+  prodTabDisplayOrder,
+  prodTabIds,
+  validateUniqueTabDisplayOrder,
+  validateTabFieldDisplayOrder
+};
