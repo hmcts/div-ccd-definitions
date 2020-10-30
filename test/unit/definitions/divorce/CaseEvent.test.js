9efdd40270947c4ec298208fd773195c2aaa601e
JackMurphy <jackmur@kainos.com>
2020-10-30 15:43:53 +0000

    DIV-6174: Add basic file structure

--- test/unit/definitions/divorce/CaseEvent.test.js
+++ test/unit/definitions/divorce/CaseEvent.test.js
@@ -39,6 +39,7 @@ describe('CaseEvent', () => {
         'CaseEvent-general-email-nonprod',
         'CaseEvent-general-order-nonprod',
         'CaseEvent-general-referral-nonprod',
+        'CaseEvent-served-by-alternative-method-nonprod',
         'CaseEvent-nonprod'
       ]);
 
