d93fd5aaf31d4b4da3434aa77f4cb59c31095ba0
jackMur <jackmur@kainos.com>
2021-01-20 15:00:13 +0000

    Pair programming - update CaseEventToComplexTypes

--- test/unit/definitions/divorce/GenericValidation.test.js
+++ test/unit/definitions/divorce/GenericValidation.test.js
@@ -10,7 +10,7 @@ const Config = {
   AuthorisationCaseType: Object.assign(require('definitions/divorce/json/AuthorisationCaseType/AuthorisationCaseType'), []),
   AuthorisationComplexType: Object.assign(require('definitions/divorce/json/AuthorisationComplexType/AuthorisationComplexType'), []),
   CaseEvent: Object.assign(require('definitions/divorce/json/CaseEvent/CaseEvent'), []),
-  CaseEventToComplexTypes: Object.assign(require('definitions/divorce/json/CaseEventToComplexTypes/CaseEventToComplexTypes'), []),
+  CaseEventToComplexTypes: Object.assign(require('definitions/divorce/json/CaseEventToComplexTypes/CaseEventToComplexTypes-share-a-case-nonprod.json'), []),
   CaseEventToFields: Object.assign(require('definitions/divorce/json/CaseEventToFields/CaseEventToFields'), []),
   CaseField: Object.assign(require('definitions/divorce/json/CaseField/CaseField'), []),
   CaseRoles: Object.assign(require('definitions/divorce/json/CaseRoles'), []),
