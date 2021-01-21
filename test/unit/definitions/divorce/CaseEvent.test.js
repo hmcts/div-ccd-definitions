9594783d219c5ba7a9d307d2a0f19c852e588f8c
Felix Eyetan <peyetan@yahoo.com>
2021-01-21 10:05:32 +0000

    Adding new test cases

--- test/unit/definitions/divorce/CaseEvent.test.js
+++ test/unit/definitions/divorce/CaseEvent.test.js
@@ -1,13 +1,11 @@
 const { expect } = require('chai');
 const { uniqWith } = require('lodash');
-const {
-  isNotEmpty, isNotLongerThan, noDuplicateFound,
-  whenPopulated, loadAllFiles
+const { SHORT_STRING,
+        MEDIUM_STRING,
+        LONG_STRING,
+        isNotEmpty, isNotLongerThan, noDuplicateFound,
+        whenPopulated, loadAllFiles
 } = require('../../utils/utils');
-
-const SHORT_STRING = 30;
-const MEDIUM_STRING = 70;
-const LONG_STRING = 100;
 
 const getCaseEventDefinitions = loadAllFiles('CaseEvent');
 
