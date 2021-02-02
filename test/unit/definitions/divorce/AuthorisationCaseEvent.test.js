e7d2bae1e3ff8fd4d62082c13b2d5b3d67480a50
jackMur <jackmur@kainos.com>
2021-02-02 11:22:19 +0000

    24.1 'solAosNotReceivedFromServiceAppRejected - Demoted permissions to 'R'

--- test/unit/definitions/divorce/AuthorisationCaseEvent.test.js
+++ test/unit/definitions/divorce/AuthorisationCaseEvent.test.js
@@ -16,24 +16,25 @@ describe('AuthorisationCaseEvent', () => {
     before(() => {
       nonProd = getAuthorisationCaseEventDefinitions([
         'AuthorisationCaseEvent',
-        'AuthorisationCaseEvent-alternative-service-nonprod',
+        'AuthorisationCaseEvent-nonprod',
         'AuthorisationCaseEvent-alt-service-process-server-nonprod',
+        'AuthorisationCaseEvent-alternative-service-nonprod',
         'AuthorisationCaseEvent-amend-court-orders-nonprod',
         'AuthorisationCaseEvent-deemed-and-dispensed-nonprod',
         'AuthorisationCaseEvent-general-email-nonprod',
         'AuthorisationCaseEvent-general-referral-nonprod',
-        'AuthorisationCaseEvent-nonprod'
+        'AuthorisationCaseEvent-resp-journey-roles-and-permissions-nonprod'
       ]);
 
       allEventsForNonProd = getCaseEventDefinitions([
         'CaseEvent',
+        'CaseEvent-nonprod',
         'CaseEvent-alt-service-process-server-nonprod',
         'CaseEvent-alternative-service-nonprod',
         'CaseEvent-amend-court-orders-nonprod',
         'CaseEvent-deemed-and-dispensed-nonprod',
         'CaseEvent-general-email-nonprod',
-        'CaseEvent-general-referral-nonprod',
-        'CaseEvent-nonprod'
+        'CaseEvent-general-referral-nonprod'
       ]);
     });
 
