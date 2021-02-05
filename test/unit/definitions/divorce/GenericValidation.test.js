edc304984037cd6e365646fd7571578e6ffdbccd
Patryk Jar <jar.patryk@gmail.com>
2021-02-05 16:58:09 +0100

    GenericValidation.test

--- test/unit/definitions/divorce/GenericValidation.test.js
+++ test/unit/definitions/divorce/GenericValidation.test.js
@@ -2,55 +2,103 @@
 
 const expect = require('chai').expect;
 const { uniq } = require('lodash');
+const { prod, nonprod } = require('../../utils/dataProvider');
 
 const Config = {
-  AuthorisationCaseEvent: Object.assign(require('definitions/divorce/json/AuthorisationCaseEvent/AuthorisationCaseEvent'), []),
-  AuthorisationCaseField: Object.assign(require('definitions/divorce/json/AuthorisationCaseField/AuthorisationCaseField'), []),
-  AuthorisationCaseState: Object.assign(require('definitions/divorce/json/AuthorisationCaseState/AuthorisationCaseState'), []),
-  AuthorisationCaseType: Object.assign(require('definitions/divorce/json/AuthorisationCaseType/AuthorisationCaseType'), []),
-  AuthorisationComplexType: Object.assign(require('definitions/divorce/json/AuthorisationComplexType/AuthorisationComplexType'), []),
-  CaseEvent: Object.assign(require('definitions/divorce/json/CaseEvent/CaseEvent'), []),
-  CaseEventToComplexTypes: Object.assign(require('definitions/divorce/json/CaseEventToComplexTypes/CaseEventToComplexTypes'), []),
-  CaseEventToFields: Object.assign(require('definitions/divorce/json/CaseEventToFields/CaseEventToFields'), []),
-  CaseField: Object.assign(require('definitions/divorce/json/CaseField/CaseField'), []),
-  CaseRoles: Object.assign(require('definitions/divorce/json/CaseRoles'), []),
-  CaseType: Object.assign(require('definitions/divorce/json/CaseType'), []),
-  CaseTypeTab: Object.assign(require('definitions/divorce/json/CaseTypeTab/CaseTypeTab'), []),
-  ComplexTypes: Object.assign(require('definitions/divorce/json/ComplexTypes/ComplexTypes'), []),
-  FixedLists: Object.assign(require('definitions/divorce/json/FixedLists/FixedLists'), []),
-  Jurisdiction: Object.assign(require('definitions/divorce/json/Jurisdiction'), []),
-  SearchAlias: Object.assign(require('definitions/divorce/json/SearchAlias'), []),
-  SearchInputFields: Object.assign(require('definitions/divorce/json/SearchInputFields'), []),
-  SearchResultFields: Object.assign(require('definitions/divorce/json/SearchResultFields'), []),
-  State: Object.assign(require('definitions/divorce/json/State/State'), []),
-  UserProfile: Object.assign(require('definitions/divorce/json/UserProfile'), []),
-  WorkBasketInputFields: Object.assign(require('definitions/divorce/json/WorkBasketInputFields/WorkBasketInputFields'), []),
-  WorkBasketResultFields: Object.assign(require('definitions/divorce/json/WorkBasketResultFields'), [])
+  nonprod: {
+    AuthorisationCaseEvent: nonprod.AuthorisationCaseEvent,
+    AuthorisationCaseField: nonprod.AuthorisationCaseField,
+    AuthorisationCaseState: nonprod.AuthorisationCaseState,
+    AuthorisationCaseType: Object.assign(require('definitions/divorce/json/AuthorisationCaseType/AuthorisationCaseType'), []),
+    AuthorisationComplexType: nonprod.AuthorisationComplexType,
+    CaseEvent: nonprod.CaseEvent,
+    CaseEventToComplexTypes: nonprod.CaseEventToComplexTypes,
+    CaseEventToFields: nonprod.CaseEventToFields,
+    CaseField: nonprod.CaseField,
+    CaseRoles: Object.assign(require('definitions/divorce/json/CaseRoles'), []),
+    CaseType: Object.assign(require('definitions/divorce/json/CaseType'), []),
+    CaseTypeTab:  nonprod.CaseTypeTab,
+    ComplexTypes: Object.assign(require('definitions/divorce/json/ComplexTypes/ComplexTypes'), []),
+    FixedLists: Object.assign(require('definitions/divorce/json/FixedLists/FixedLists'), []),
+    Jurisdiction: Object.assign(require('definitions/divorce/json/Jurisdiction'), []),
+    SearchAlias: Object.assign(require('definitions/divorce/json/SearchAlias'), []),
+    SearchInputFields: Object.assign(require('definitions/divorce/json/SearchInputFields'), []),
+    SearchResultFields: Object.assign(require('definitions/divorce/json/SearchResultFields'), []),
+    State: nonprod.State,
+    UserProfile: Object.assign(require('definitions/divorce/json/UserProfile'), []),
+    WorkBasketInputFields: Object.assign(require('definitions/divorce/json/WorkBasketInputFields/WorkBasketInputFields'), []),
+    WorkBasketResultFields: Object.assign(require('definitions/divorce/json/WorkBasketResultFields'), [])
+  },
+  prod: {
+    AuthorisationCaseEvent: prod.AuthorisationCaseEvent,
+    AuthorisationCaseField: prod.AuthorisationCaseField,
+    AuthorisationCaseState: prod.AuthorisationCaseState,
+    AuthorisationCaseType: Object.assign(require('definitions/divorce/json/AuthorisationCaseType/AuthorisationCaseType'), []),
+    AuthorisationComplexType: prod.AuthorisationComplexType,
+    CaseEvent: prod.CaseEvent,
+    CaseEventToComplexTypes: prod.CaseEventToComplexTypes,
+    CaseEventToFields: prod.CaseEventToFields,
+    CaseField: prod.CaseField,
+    CaseRoles: Object.assign(require('definitions/divorce/json/CaseRoles'), []),
+    CaseType: Object.assign(require('definitions/divorce/json/CaseType'), []),
+    CaseTypeTab:  prod.CaseTypeTab,
+    ComplexTypes: Object.assign(require('definitions/divorce/json/ComplexTypes/ComplexTypes'), []),
+    FixedLists: Object.assign(require('definitions/divorce/json/FixedLists/FixedLists'), []),
+    Jurisdiction: Object.assign(require('definitions/divorce/json/Jurisdiction'), []),
+    SearchAlias: Object.assign(require('definitions/divorce/json/SearchAlias'), []),
+    SearchInputFields: Object.assign(require('definitions/divorce/json/SearchInputFields'), []),
+    SearchResultFields: Object.assign(require('definitions/divorce/json/SearchResultFields'), []),
+    State: prod.State,
+    UserProfile: Object.assign(require('definitions/divorce/json/UserProfile'), []),
+    WorkBasketInputFields: Object.assign(require('definitions/divorce/json/WorkBasketInputFields/WorkBasketInputFields'), []),
+    WorkBasketResultFields: Object.assign(require('definitions/divorce/json/WorkBasketResultFields'), [])
+  }
+
 };
 
-describe('For each config sheet', () => {
-  it('should have unique rows', () => {
-    Object.keys(Config).forEach(sheetName => {
-      const originalContent = Config[sheetName];
-      const uniqueList = uniq(originalContent);
-      expect(uniqueList.length).to.eq(originalContent.length);
-    });
+function uniqueRows (config) {
+  Object.keys(config).forEach(sheetName => {
+    const originalContent = config[sheetName];
+    const uniqueList = uniq(originalContent);
+    expect(uniqueList.length).to.eq(originalContent.length);
   });
+}
 
-  it('should not have any special characters, tabs or line breaks in any of the priority user fields', () => {
-    const accepted = /^[\w|*|\-|.|[|\]]+$/;
-    const priorityUserFields = ['CaseFieldID', 'CaseStateID', 'ID', 'CaseEventID'];
-    Object.keys(Config).forEach(sheetName => {
-      const content = Config[sheetName];
-      content.forEach(row => {
-        priorityUserFields.forEach(field => {
-          const cellValue = row[field];
-          if (cellValue && !cellValue.match(accepted)) {
-            console.log(`Cell ${field} value in sheet ${sheetName} has unexpected characters for value ${cellValue}.`);
-            expect(cellValue.toString()).to.match(accepted);
-          }
-        });
+function validateNames (config) {
+  const accepted = /^[\w|*|\-|.|[|\]]+$/;
+  const priorityUserFields = ['CaseFieldID', 'CaseStateID', 'ID', 'CaseEventID'];
+  Object.keys(config).forEach(sheetName => {
+    const content = config[sheetName];
+    content.forEach(row => {
+      priorityUserFields.forEach(field => {
+        const cellValue = row[field];
+        if (cellValue && !cellValue.match(accepted)) {
+          console.log(`Cell ${field} value in sheet ${sheetName} has unexpected characters for value ${cellValue}.`);
+          expect(cellValue.toString()).to.match(accepted);
+        }
       });
     });
   });
+}
+
+describe('For each config sheet', () => {
+  context("nonprod", () => {
+    it('should have unique rows', () => {
+      uniqueRows(Config.nonprod);
+    });
+
+    it('should not have any special characters, tabs or line breaks in any of the priority user fields', () => {
+      validateNames(Config.nonprod);
+    });
+  })
+
+  context("nonprod", () => {
+    it('should have unique rows', () => {
+      uniqueRows(Config.prod);
+    });
+
+    it('should not have any special characters, tabs or line breaks in any of the priority user fields', () => {
+      validateNames(Config.prod);
+    });
+  })
 });
