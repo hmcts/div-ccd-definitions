fd74de8cd66acdc22d512b24575cd876929f83ea
Patryk Jar <jar.patryk@gmail.com>
2021-02-09 12:52:49 +0100

    FieldTypeParameter check

--- test/unit/definitions/divorce/CaseField.test.js
+++ test/unit/definitions/divorce/CaseField.test.js
@@ -17,8 +17,8 @@ function assertFieldDefinitionIsValid (row) {
   expect(row.Label).to.be.a('string').and.satisfy(isNotEmpty());
   expect(row.SecurityClassification).to.eq('Public');
   expect(row.FieldType).to.be.a('string').and.satisfy(isNotLongerThan(MEDIUM_STRING));
-  if(row.FieldType === 'Collection') {
-    expect(row.FieldTypeParameter).to.be.a('string').and.satisfy(isNotEmpty());
+  if(row.FieldType === 'Collection' || row.FieldType === 'FixedList' ||
+    row.FieldType === 'FixedRadioList' || row.FieldType === 'MultiSelectList') {
     expect(row.FieldTypeParameter).to.be.a('string').and.satisfy(isNotEmpty());
   }
 }
