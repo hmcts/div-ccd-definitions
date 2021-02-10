d54ba9cd75991e47060e3b064b04b4f474c3c6b6
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-02-10 15:59:36 +0000

    Add UpdatePetSolContactDetails event and page

--- test/unit/utils/dataProvider.js
+++ test/unit/utils/dataProvider.js
@@ -81,6 +81,7 @@ module.exports = {
         'AuthorisationCaseEvent-deemed-and-dispensed-nonprod',
         'AuthorisationCaseEvent-general-email-nonprod',
         'AuthorisationCaseEvent-general-referral-nonprod',
+        'AuthorisationCaseEvent-resp-journey-roles-and-permissions-nonprod',
         'AuthorisationCaseEvent-nonprod'
       ]
     ),
@@ -111,6 +112,7 @@ module.exports = {
         'CaseEvent-general-email-nonprod',
         'CaseEvent-general-referral-nonprod',
         'CaseEvent-share-a-case-nonprod',
+        'AuthorisationCaseEvent-resp-journey-roles-and-permissions-nonprod',
         'CaseEvent-nonprod'
       ]),
     CaseEventToFields: getCaseEventToFieldsDefinitions([
