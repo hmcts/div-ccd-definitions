2d42b3d5662bfcf3bbe7e557ab25bedb29f4df95
dawudgovuk <dawud.rahman@justice.gov.uk>
2023-03-06 10:51:28 +0000

    DFR-1777-Yarn3-upgrade - update yarn scripts and batch files

--- bin/generate-bulk-excel-preview.sh
+++ bin/generate-bulk-excel-preview.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 source default.env
-DIV_ENV=preview CCD_DEF_COS_URL=$COS_API_URL CCD_DEF_CCD_URL=$CCD_DATA_API_URL yarn run generate-bulk-excel
+DIV_ENV=preview
+CCD_DEF_COS_URL=$COS_API_URL
+CCD_DEF_CCD_URL=$CCD_DATA_API_URL
+pushd ccd-definition-processor && yarn --cwd ccd-definition-processor json2xlsx -D ../definitions/bulk-action/json -o ../definitions/bulk-action/xlsx/ccd-div-bulk-action-config-${DIV_ENV:-base}.xlsx
--- bin/generate-excel-local.sh
+++ bin/generate-excel-local.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 source default.env
-DIV_ENV=local CCD_DEF_COS_URL=$npm_package_config_local_cosUrl CCD_DEF_CCD_URL=$npm_package_config_local_ccdUrl yarn run generate-excel -e *-prod.json
+DIV_ENV=local
+CCD_DEF_COS_URL=$npm_package_config_local_cosUrl
+CCD_DEF_CCD_URL=$npm_package_config_local_ccdUrl
+pushd ccd-definition-processor && yarn --cwd ccd-definition-processor json2xlsx -D ../definitions/divorce/json -o ../definitions/divorce/xlsx/ccd-config-${DIV_ENV:-base}.xlsx -e *-prod.json
