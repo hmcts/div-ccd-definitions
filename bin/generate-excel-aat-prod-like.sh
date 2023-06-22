839dfa9d411887db0b9b6ca3551d05e0cabd6fa0
dawudgovuk <dawud.rahman@justice.gov.uk>
2023-06-22 10:48:53 +0100

    Error message on Referrals and General orders - old law

--- bin/generate-excel-aat-prod-like.sh
+++ bin/generate-excel-aat-prod-like.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source default.env
-DIV_ENV=demo-prod-like CCD_DEF_COS_URL=$npm_package_config_demo_cosUrl CCD_DEF_CCD_URL=$npm_package_config_demo_ccdUrl EXCLUDE='*-nonprod.json' yarn run generate-excel
+DIV_ENV=aat-prod-like CCD_DEF_COS_URL=$npm_package_config_aat_cosUrl CCD_DEF_CCD_URL=$npm_package_config_aat_ccdUrl EXCLUDE='*-nonprod.json' yarn run generate-excel
