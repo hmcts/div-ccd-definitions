1e034ede013a269bdd5de701884b0cb74415ccbd
dawudgovuk <dawud.rahman@justice.gov.uk>
2023-07-04 14:49:44 +0100

    Fix bug in CCD Excludes

--- bin/generate-excel-prod.sh
+++ bin/generate-excel-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source default.env
-DIV_ENV=prod CCD_DEF_COS_URL=$npm_package_config_prod_cosUrl CCD_DEF_CCD_URL=$npm_package_config_prod_ccdUrl EXCLUDE='*-prod.json' yarn run generate-excel
+DIV_ENV=prod CCD_DEF_COS_URL=$npm_package_config_prod_cosUrl CCD_DEF_CCD_URL=$npm_package_config_prod_ccdUrl EXCLUDE='*-nonprod.json' yarn run generate-excel
